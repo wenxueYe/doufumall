996cf38aebbb561db513e9818489d1ae041ae3e9
yewenxue <yewenxuewy@163.com>
2022-02-02 10:57:05 +0800

    简单的配置中心

--- mall-member/src/main/java/org/ywx/member/controller/Test.java
+++ mall-member/src/main/java/org/ywx/member/controller/Test.java
@@ -1,6 +1,8 @@
 package org.ywx.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.ywx.common.utils.R;
@@ -13,11 +15,16 @@ import org.ywx.member.feign.CouponFeignService;
  * @File : Test
  * @Project : doufumall
  */
+@RefreshScope
 @RestController
 @RequestMapping("member/test")
 public class Test {
     @Autowired
     private CouponFeignService couponFeignService;
+
+    @Value("${user.name}")
+    private String username;
+
 
     /**
      * 会员的优惠券信息 -- cus
@@ -25,7 +32,7 @@ public class Test {
     @RequestMapping("/coupons")
     public R test() {
         MemberEntity memberEntity = new MemberEntity();
-        memberEntity.setNickname("张三");
+        memberEntity.setNickname(username);
         R menberCoupons = couponFeignService.memberCoupons();
         return R.ok().put("member",memberEntity).put("coupons",menberCoupons.get("coupons"));
     }
