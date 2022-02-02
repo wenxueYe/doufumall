c0c7dc5446ee5c4c32061116361d22f81e71ac9f
yewenxue <yewenxuewy@163.com>
2022-02-02 12:27:04 +0800

    网关路由

---
+++ mall-gateway/src/test/java/org/ywx/mallgateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ywx.mallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
